8c4d8870effa51a795002a55ae9598a2d0992991
billchen-818 <chenmingxing001@gmail.com>
2023-11-02 13:19:21 +0800

    add test

--- src/UniswapV2Pair.sol
+++ src/UniswapV2Pair.sol
@@ -4,6 +4,12 @@ pragma solidity ^0.8.0;
 import {ERC20} from "solmate/tokens/ERC20.sol";
 import {Math} from "./libraries/Math.sol";
 
+interface IERC20 {
+    function balanceOf(address) external returns (uint256);
+}
+
+error InsufficientLiquidityMinted();
+
 contract UniswapV2Pair is ERC20, Math {
     // 定义常量 最小的流动性
     uint256 constant MINIMUM_LIQUIDITY = 1000;
@@ -16,8 +22,46 @@ contract UniswapV2Pair is ERC20, Math {
     uint256 private reserve0;
     uint256 private reserve1;
 
+    event Mint(address, uint256, uint256);
+    event Sync(uint256, uint256);
+
     constructor(address _token0, address _token1) ERC20("ZuniswapV2 Pair", "ZUNIV2", 18) {
         token0 = _token0;
         token1 = _token1;
+    }
+
+    function mint() public {
+        uint256 balance0 = IERC20(token0).balanceOf(address(this));
+        uint256 balance1 = IERC20(token1).balanceOf(address(this));
+        uint256 amount0 = balance0 - reserve0;
+        uint256 amount1 = balance1 - reserve1;
+        uint256 liquidity;
+
+        if (totalSupply == 0) {
+            liquidity = Math.sqrt(amount0 * amount1) - MINIMUM_LIQUIDITY;
+            _mint(address(0), MINIMUM_LIQUIDITY);
+        } else {
+            liquidity = Math.min(
+                (amount0 * totalSupply) / reserve0,
+                (amount1 * totalSupply) / reserve1
+            );
+        }
+
+        if (liquidity <= 0) {
+            revert InsufficientLiquidityMinted();
+        }
+
+        _mint(msg.sender, liquidity);
+
+        _update(balance0, balance1);
+
+        emit Mint(msg.sender, amount0, amount1);
+    }
+
+    function _update(uint256 balance0, uint256 balance1)  private {
+        reserve0 = balance0;
+        reserve1 = balance1;
+
+        emit Sync(reserve0, reserve1);
     }
 }
---
+++ test/UniswapV2Pair.t.sol
@@ -0,0 +1,33 @@
+// SPDX-License-Identifier: SEE LICENSE IN LICENSE
+pragma solidity ^0.8.0;
+
+import {Test, console} from "forge-std/Test.sol";
+import "./mocks/ERC20Mintable.sol";
+import "../src/UniswapV2Pair.sol";
+
+contract UniswapV2PairTest is Test {
+    ERC20Mintable token0;
+    ERC20Mintable token1;
+    UniswapV2Pair pair;
+
+    function setUp() public {
+        token0 = new ERC20Mintable("Token A", "TKNA");
+        token1 = new ERC20Mintable("Token B", "TKNB");
+
+        pair = new UniswapV2Pair(address(token0), address(token1));
+
+        token0.mint(10 ether, address(this));
+        token1.mint(10 ether, address(this));
+    }
+
+    function testMintBootstrap() public {
+        token0.transfer(address(pair), 1 ether);
+        token1.transfer(address(pair), 1 ether);
+
+        pair.mint();
+
+        assertEq(pair.balanceOf(address(this)), 1 ether - 1000);
+        assertEq(pair.totalSupply(), 1 ether);
+    }
+
+}
